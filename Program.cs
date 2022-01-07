aedac75b93587c1956fde3a49ba116ca9ff1be6d
utsav52 <utsavmaniyar23@gmail.com>
2022-01-07 13:55:17 +0530

    Submit Practical2

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace practical_3
+{
+    class CourierCompany
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter the weight of parcel: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter type of distance");
+            int distance = Convert.ToInt32(Console.ReadLine());
+
+            int charge = 0;
+
+            if (distance <= 100)
+            {
+                charge = 50;
+            }
+            else if (distance <= 200)
+                charge = 65;
+            else if (distance <= 300)
+                charge = 90;
+            else
+                charge = 120;
+
+            int total_amount = weight * charge;
+
+            Console.WriteLine("Total Amount =" +
+                " " + total_amount);
+
+            Console.Read();
+        }
+    }
+}
